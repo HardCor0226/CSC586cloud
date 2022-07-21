a4a8e249bf2c86fa7b845bb5d4bdd38aca2fded1
Corey Strunk <70726393+HardCor0226@users.noreply.github.com>
2022-07-21 19:48:41 -0400

    Create observer_setup.sh

---
+++ observer_setup.sh
@@ -0,0 +1,11 @@
+!/bin/bash
+set -x
+
+sudo apt-get install -y nfs-common
+sudo apt-get update
+
+mkdir webserver_log
+
+sudo mount 192.168.1.1:/share/log webserver_log
+cd webserver_log
+sudo touch observer_1
