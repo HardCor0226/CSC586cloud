b81448862b040273e5ab7c6c3a8721586878cd34
Corey Strunk <70726393+HardCor0226@users.noreply.github.com>
2022-07-27 12:42:05 -0400

    Update observer_setup.sh

--- observer_setup.sh
+++ observer_setup.sh
@@ -4,9 +4,9 @@ set -x
 sudo apt-get install -y nfs-common
 sudo apt-get update
 
-mkdir webserver_log
+mkdir /var/webserver_log
 
 sleep 1m
-sudo mount 192.168.1.1:/share/log webserver_log
-cd webserver_log
+sudo mount 192.168.1.1:/share/log /var/webserver_log
+cd /var/webserver_log
 sudo touch observer_1
