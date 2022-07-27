fd57a57d79a1c72b1f2b9724d9e311c140d44e0e
Corey Strunk <70726393+HardCor0226@users.noreply.github.com>
2022-07-27 18:25:38 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-sudo cat /var/log/auth.log | grep -i disconnected | awk '{print 1" "$2" "$3" "$11}' > modify.log
-while IFS= read -r line; do
-        ipAdd=` echo $line |awk '{print $4}'`
-                date =`echo $line | awk '{print $1 $2 $3}'`
-echo "$ipAdd,$(curl -s https://json.geoiplookup.io/$ipAdd | jq ' .country_name'),$date" &>> /var/webserver_log/unauthorized.log
-done < "modify.log"
+sudo apt-get install geoip-bin > /dev/null
+array=()
+grep "Invalid user" /var/log/auth.log >> holding.log
+readarray array < holding.log
+ip=""
+country=""
+date=""
+for i in "${array[@]}"; do
+        ip="$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' <<< "$i")"
+        country="$(geoiplookup -s $ip)"
+        date=$(echo "$i" | cut -c1-15)
+        echo "$ip ""$country ""$date" >> /var/webserver_log/unauthorized.log
+done
+sudo rm holding.log
