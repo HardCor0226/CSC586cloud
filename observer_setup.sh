decc343e4334598434ac04ddf15f820925522bca
Corey Strunk <70726393+HardCor0226@users.noreply.github.com>
2022-07-27 14:12:16 -0400

    Update observer_setup.sh

--- observer_setup.sh
+++ observer_setup.sh
@@ -7,6 +7,6 @@ sudo apt-get update
 mkdir /var/webserver_monitor
 sudo chown nobody:nogroup /var/webserver_monitor
 
-sudo mount 192.168.1.1:/share/log /var/webserver_monitor
+sudo mount 192.168.1.1:/var/webserver_log /var/webserver_monitor
 cd /var/webserver_monitor
 sudo touch observer_1
