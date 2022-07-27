aa574efd637ff511d1fe6deb3281f5643ef5ebc5
Corey Strunk <70726393+HardCor0226@users.noreply.github.com>
2022-07-27 17:43:39 -0400

    Update observer_setup.sh

--- observer_setup.sh
+++ observer_setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -x
 
 sudo apt-get install -y nfs-common
@@ -9,3 +8,5 @@ sudo mkdir /var/webserver_monitor
 sudo chown nobody:nogroup /var/webserver_monitor
 
 sudo mount 192.168.1.1:/var/webserver_log /var/webserver_monitor
+cd /var/webserver_monitor
+sudo touch unauthorized.log
