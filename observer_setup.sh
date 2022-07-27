099993cac1c766f7160933c212c18459e659633c
Corey Strunk <70726393+HardCor0226@users.noreply.github.com>
2022-07-27 13:41:52 -0400

    Update observer_setup.sh

--- observer_setup.sh
+++ observer_setup.sh
@@ -5,8 +5,8 @@ sudo apt-get install -y nfs-common
 sudo apt-get update
 
 mkdir /var/webserver_log
+sudo chown nobody:nogroup /var/webserver_log
 
-sleep 1m
 sudo mount 192.168.1.1:/share/log /var/webserver_log
 cd /var/webserver_log
 sudo touch observer_1
